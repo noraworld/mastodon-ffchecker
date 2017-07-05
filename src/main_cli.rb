bc6bcd2113ca582acb30046216f58e62134051c7
noraworld <noraworld@mail.com>
2017-07-06 06:01:41 +0900

    :pencil2: Change main_with_relationships.rb to main_cli.rb
    - main_cli.rb is now for command line interface

--- src/main_cli.rb
+++ src/main_cli.rb
@@ -27,36 +27,14 @@ s = f.read
 f.close
 excluded_users = JSON.parse(s)['users']
 
-if not_followed_ids.empty?
-  return
-end
-
-username = client.account(id).acct
-not_followed_list = ""
+not_followed_accts = []
 
 not_followed_ids.each do |nf|
   nf = client.account(nf)
-  not_followed_acct = nf.acct
-
-  unless excluded_users.include? not_followed_acct
-    if nf.display_name.empty?
-      not_followed_list += "- " + nf.username + " (" + nf.acct + ")\n"
-    else
-      not_followed_list += "- " + nf.display_name + " (" + nf.acct + ")\n"
-    end
-  end
+  not_followed_accts.push nf.acct
 end
 
-if not_followed_list.empty?
-  return
+not_followed_list = not_followed_accts - excluded_users
+not_followed_list.each do |nf|
+  puts nf
 end
-
-if not_followed_ids.length === 1
-  message = 'The following user is followed by you, but not following you.'
-elsif not_followed_ids.length >= 2
-  message = 'The following users are followed by you, but not following you.'
-end
-
-text = "@" + username + " " + message + "\n\n" + not_followed_list
-
-client.create_status(text, nil, [], 'direct')
